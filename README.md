c60fa0d41cf910f73da2b263a70723dd132e2551
lidong1998 <dong.li98@berkeley.edu>
2019-03-20 09:56:49 +0800

    build project successfully

---
+++ README.md
@@ -0,0 +1,3 @@
+## PCL Toy Project
+This is my toy project for PCL(Point Cloud Library).
+I will add some basic PCL tutorial projects that from offical docs to this repo.
---
+++ cmake-build-debug/CMakeFiles/PCLTest1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCLTest1.dir/main.cpp.o"
+  "PCLTest1.pdb"
+  "PCLTest1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCLTest1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
